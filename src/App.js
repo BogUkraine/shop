0e31dd154bf05cb828a7ceb32a967cec521dc629
Bohdan Lysov <BogUkraine@gmail.com>
2020-03-14 19:14:54 +0200

    init commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import './styles/main.scss';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header/>
+      <Route exact path="/" component={Home}/>
+
+      <Footer/>
+    </BrowserRouter>
+  );
+}
+
+export default App;
